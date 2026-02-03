960d8e3c355aac89fb6999b0eb58f614462eae67
theahmadzada <ahmad.ahmadzada999@gmail.com>
2026-02-03 22:46:17 +0100

    add services to AppHost

--- host/Procat.AppHost/AppHost.cs
+++ host/Procat.AppHost/AppHost.cs
@@ -1,5 +1,16 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var webapi = builder.AddProject<Projects.Procat_WebAPI>("webapi");
+var psql = builder.AddPostgres("psql")
+    .WithDataVolume()
+    .AddDatabase("user-module");
+
+var minio = builder.AddMinioContainer("minio")
+    .WithDataVolume();
+
+builder.AddProject<Projects.Procat_WebAPI>("webapi")
+    .WaitFor(psql)
+    .WithReference(psql)
+    .WaitFor(minio)
+    .WithReference(minio);
 
 builder.Build().Run();
